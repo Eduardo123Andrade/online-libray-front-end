f73929e532d5ad9a6158ddf15c4717ef06450e72
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2021-10-29 14:48:19 -0300

    add create book form on home

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import { Card } from '../../components/card/card'
 import { ListItem } from '../../components/list-item/list-item'
+import { RegisterBookForm } from '../../forms/register-book/registerBook'
 import { useGetRequest } from '../../hooks/useGetRequest'
 import { Book } from '../../types/book.type'
 import './home.css'
 
 
 export const Home = () => {
-    const {data: books} = useGetRequest('/book/list-books')
+	const { data: books = [] } = useGetRequest('/book/list-books')
 
-    const renderItem = (item: Book) => {
-        const { id } = item
-        return <ListItem key={id} book={item} />
-    }
+	const renderItem = (item: Book) => {
+		const { id } = item
+		return <ListItem key={id} book={item} />
+	}
 
-    return (
-        <div className="Home">
-            <Card>
-                {books?.map(renderItem)}
-            </Card>
-        </div>
-    )
+	return (
+		<div className="Home">
+		
+			<div className='RegisterBookContainer'>
+				<Card>
+					<RegisterBookForm />
+				</Card>
+			</div>
+			
+			<div className='ListBookContainer'>
+				<Card>
+					
+					{books.map(renderItem)}
+				</Card>
+			</div>
+		
+		</div>
+	)
 }
