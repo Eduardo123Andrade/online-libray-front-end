334f3f5c1e8a095e40fff6b49cf4714d5c77971b
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2021-10-29 22:54:40 -0300

    add typing in useGetRequest

--- src/hooks/useGetRequest.tsx
+++ src/hooks/useGetRequest.tsx
@@ -1,14 +1,15 @@
-import { AxiosRequestConfig } from "axios"
+import { AxiosError, AxiosRequestConfig } from "axios"
 import { useQuery } from "react-query"
 import { useApi } from "../api/useAPI"
 
 
-export const useGetRequest = (url: string, configs?: AxiosRequestConfig): any => {
+export const useGetRequest = (url: string, configs?: AxiosRequestConfig<any>) => {
     const API = useApi()
 
-    const {data, ...rest} = useQuery({
+    const {data, ...rest} = useQuery<any, AxiosError, any, any>({
         queryKey: [url, configs],
         queryFn: () => API.get(url, configs),
+        
     })
 
     return {
