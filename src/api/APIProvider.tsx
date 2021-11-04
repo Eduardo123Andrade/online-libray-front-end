533d35199fed1f2a6fb81e735ff837cb275c93b4
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2021-11-03 21:53:27 -0300

    uptade to using base_url

--- src/api/APIProvider.tsx
+++ src/api/APIProvider.tsx
@@ -1,19 +1,21 @@
 import Axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 import React, { createContext, useMemo } from 'react'
+import { EnvironmentKeys } from '../config'
 
 export const APIContext = createContext({} as AxiosInstance)
 
 interface Props {
     children?: JSX.Element | JSX.Element[];
-    config?: AxiosRequestConfig
 }
 
-export const APIProvider = ({ config, ...props }: Props) => {
-    const { baseURL } = config ?? {}
+export const APIProvider = ({ ...props }: Props) => {
 
-    const API = useMemo(() => Axios.create({
-        baseURL: baseURL ?? 'http://localhost:3001/v1',
-    }), [baseURL])
+    const API = useMemo(() => {
+        const config: AxiosRequestConfig = {
+            baseURL: EnvironmentKeys.BASE_URL
+        }
+        return Axios.create(config)
+    }, [])
 
     return (
         <APIContext.Provider {...props} value={API} />
